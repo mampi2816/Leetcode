bd6db474d5657b5fd807d9518709b3c54997de8d
Mampi Bhattacharya <78912146+mampi2816@users.noreply.github.com>
2024-10-06 00:59:38 +0530

    Time: 2 ms (58.56%) | Memory: 43 MB (47.95%) - LeetSync

---
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int x=0;
+        x = nums2[0] - nums1[0];
+        
+        return x;
+    }
+}
